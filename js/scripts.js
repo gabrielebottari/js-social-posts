0407e428e3385b365c39e854c8d9cc9a7bf2a87c
gabrielebottari <gabriele.bottari1@gmail.com>
2023-12-20 15:13:26 +0100

    ADD milestone 2

--- js/scripts.js
+++ js/scripts.js
@@ -81,3 +81,47 @@ const posts = [
     }
 ];
 
+// Ottenere il riferimento all'elemento del DOM in cui verranno visualizzati i post
+const feedContainer = document.getElementById('feed-container');
+
+// Iterare attraverso l'array di post e generare il markup HTML per ciascun post
+posts.forEach(post => {
+  // Creare un elemento di tipo post
+  const postElement = document.createElement('div');
+  postElement.classList.add('post');
+
+  // Generare il markup HTML utilizzando i dati del post
+  postElement.innerHTML = `
+    <div class="post__header">
+        <div class="post-meta">                    
+            <div class="post-meta__icon">
+                <img class="profile-pic" src="${post.author.image}" alt="${post.author.name}">                    
+            </div>
+            <div class="post-meta__data">
+                <div class="post-meta__author">${post.author.name}</div>
+                <div class="post-meta__time">${post.created}</div>
+            </div>                    
+        </div>
+    </div>
+    <div class="post__text">${post.content}</div>
+    <div class="post__image">
+        <img src="${post.media}" alt="">
+    </div>
+    <div class="post__footer">
+        <div class="likes js-likes">
+            <div class="likes__cta">
+                <a class="like-button  js-like-button" href="#" data-postid="${post.id}">
+                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                    <span class="like-button__label">Mi Piace</span>
+                </a>
+            </div>
+            <div class="likes__counter">
+                Piace a <b id="like-counter-${post.id}" class="js-likes-counter">${post.likes}</b> persone
+            </div>
+        </div> 
+    </div>            
+  `;
+
+  // Aggiungere il post al contenitore del feed
+  feedContainer.appendChild(postElement);
+});
